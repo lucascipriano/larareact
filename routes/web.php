1de09b4b4a5afb689f01956ec79aa1bc280c8221
Lucas Cipriano <lucasncborges@gmail.com>
2025-07-31 12:07:37 -0300

    toast

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\TransactionController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
@@ -9,12 +10,10 @@ Route::get('/', function () {
 })->name('home');
 
 Route::middleware(['auth', 'verified'])->group(function () {
-//    Route::get('dashboard', function () {return Inertia::render('dashboard');})->name('dashboard');
-    Route::get('dashboard', [\App\Http\Controllers\DashboardController::class, 'index'])->name('dashboard');
-
-//    Route::get('dashboard', [\App\Http\Controllers\DashboardController::class, 'index'])->name('dashboard');
+    Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard');
+    Route::post('/transactions', [TransactionController::class, 'store'])->name('transactions.store');
+    Route::delete('transactions/{id}', [DashboardController::class, 'destroy'])->name('transactions.destroy');
 });
-Route::post('/transactions', [TransactionController::class, 'store'])->name('transactions.store');
 
 require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';
